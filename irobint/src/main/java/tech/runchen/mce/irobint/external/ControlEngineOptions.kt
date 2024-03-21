9a829e0c12e13ea5d456bd4725295c294567a92d
zzj <565547938@qq.com>
2024-03-21 10:27:32 +0800

    第一次提交

--- irobint/src/main/java/tech/runchen/mce/irobint/external/ControlEngineOptions.kt
+++ irobint/src/main/java/tech/runchen/mce/irobint/external/ControlEngineOptions.kt
@@ -1,12 +1,8 @@
 package tech.runchen.mce.irobint.external
 
-import tech.runchen.mce.slamtec.client.mqtt.MqttOptions
-import tech.runchen.mce.slamtec.client.robot.RobotOptions
 
 data class ControlEngineOptions(
     var debug: Boolean = false,
     var chassisServiceUrl: String = "http://127.0.0.1:1448/",
     var cloudServiceUrl: String = "http://127.0.0.1:8080/",
-    var robotOptions: RobotOptions,
-    var mqttOptions: MqttOptions,
 )
